4613dafc541d33ce6bbac3e2e1611c494d8ab489
CoolManTheCool <noahgrimes0915@gmail.com>
2024-03-25 20:50:30 -0500

    something

--- Assets/Gui/Splash.gd
+++ Assets/Gui/Splash.gd
@@ -1,3 +1,5 @@
+@tool
+
 extends Label
 @export var max_scale: float = 5
 @export var min_scale: float = 2
@@ -5,8 +7,14 @@ extends Label
 var time: float = 0
 var sv: float = 0 # scale vector
 var ps: float = 0 # positive sine
+var init_pos: Vector2 = Vector2(0, 0)
+@export var update: bool = false
 
 func _ready():
+	redo_text()
+	init_pos = position
+
+func redo_text():
 	var file = FileAccess.open("res://Assets/Gui/spashes.txt", FileAccess.READ)
 	var content = file.get_as_text()
 	file.close()
@@ -14,10 +22,15 @@ func _ready():
 	var random_index = rand_from_seed(Time.get_ticks_usec())[0] % (splash_text.size()-1)
 	text = splash_text[random_index]
 
-
 func _process(delta):
 	time += delta
 	ps = (1+sin(time*speed))/2
 	sv = ps*min_scale-(ps-1)*max_scale
 	scale.x = sv
 	scale.y = sv
+	
+	label_settings.font_size
+	
+	if update:
+		redo_text()
+		update = false
